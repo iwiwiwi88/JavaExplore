764faef472384cd8c6b96677ca4fea40ebe19b83
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-03-14 15:04:21 +0100

    Triangle prints correctly

---
+++ TestingSysout/src/main/java/Triangle.java
@@ -0,0 +1,34 @@
+/**
+ * Created by iwonasasak on 14.03.2018.
+ */
+public class Triangle implements TrianglePrintable {
+
+    public void printTriangle(int numberOfLevels) {
+        for (int i = 1; i <= numberOfLevels; i++) {
+            printTriangleLevel(numberOfLevels, i);
+        }
+    }
+
+    private void printTriangleLevel(int numberOfLevels, int actualLevel) {
+        printSpaces(numberOfLevels, actualLevel);
+        printStars(actualLevel);
+        System.out.println();
+    }
+
+    private void printSpaces(int numberOfLevels, int actualLevel) {
+        for (int i = 0; i < numberOfLevels - actualLevel; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    private void printStars(int actualLevel) {
+        for (int i = 0; i < 2 * actualLevel - 1; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public static void main(String[] args) {
+        Triangle t = new Triangle();
+        t.printTriangle(4);
+    }
+}
--- TestingSysout/src/main/java/TrianglePrintable.java
+++ TestingSysout/src/main/java/TrianglePrintable.java
@@ -1,17 +1,17 @@
 /**
  * Created by iwonasasak on 14.03.2018.
- *
+ * <p>
  * Abilitiy to print triangles like this:
- *
+ * <p>
  * |   *
  * |  ***
  * | *****
  * |*******
- *
+ * <p>
  * Example for numberOfLevels = 4
- *
  */
 public interface TrianglePrintable {
 
     void printTriangle(int numberOfLevels);
+
 }
