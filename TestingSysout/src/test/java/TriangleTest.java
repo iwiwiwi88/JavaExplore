6caae23cbf1d551e28d32386d51b2dda087699c9
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-03-14 15:39:43 +0100

    TestNG class for Triangle added

---
+++ TestingSysout/src/test/java/TriangleTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by iwonasasak on 14.03.2018.
+ */
+public class TriangleTest {
+
+    @Test
+    public void printTriangleForZeroLevels() {
+
+    }
+    @Test
+    public void printTriangleForOneLevel() {
+
+    }
+    @Test
+    public void printTriangleFor4Levels() {
+
+    }
+}
