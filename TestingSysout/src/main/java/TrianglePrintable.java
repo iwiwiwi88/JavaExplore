03235f940bae17c3ef72168b5c62a5130a992d6c
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-03-14 14:40:26 +0100

    interface TrianglePrintable added

---
+++ TestingSysout/src/main/java/TrianglePrintable.java
@@ -0,0 +1,17 @@
+/**
+ * Created by iwonasasak on 14.03.2018.
+ *
+ * Abilitiy to print triangles like this:
+ *
+ * |   *
+ * |  ***
+ * | *****
+ * |*******
+ *
+ * Example for numberOfLevels = 4
+ *
+ */
+public interface TrianglePrintable {
+
+    void printTriangle(int numberOfLevels);
+}
